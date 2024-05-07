e4a68996a9a38f7fd3764ef0be891886f0ab3863
vincentanu04 <vincentanu04@gmail.com>
2024-05-08 03:31:25 +0800

    add react-pdf package and tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,150 +1,174 @@
-import './App.css'
+import './App.css';
 import { useState } from 'react';
-import Buttons from './components/Buttons/Buttons'
-import { NavButton, Button } from './components/Buttons/Buttons'
-import { ProfileForm, EducationForm, ExperienceForm } from './components/Form/Form';
+import Buttons from './components/Buttons/Buttons';
+import { NavButton, Button } from './components/Buttons/Buttons';
+import {
+  ProfileForm,
+  EducationForm,
+  ExperienceForm,
+} from './components/Form/Form';
 import CV from './components/CV/CV';
+import { PDFViewer, StyleSheet } from '@react-pdf/renderer';
 
 const initialFormData = {
-    profile: {
-        name: '',
-        email: '',
-        phoneNumber: '',
-        location: '',
+  profile: {
+    name: '',
+    email: '',
+    phoneNumber: '',
+    location: '',
+  },
+  education: {
+    schoolName: '',
+    schoolLocation: '',
+    titleOfStudy: '',
+    gpa: '',
+    fromDate: '',
+    toDate: '',
+  },
+  experience: [
+    {
+      jobId: 1,
+      companyName: '',
+      location: '',
+      positionTitle: '',
+      responsibilities: [''],
+      dateFrom: '',
+      dateUntil: '',
     },
-    education: {
-        schoolName: '',
-        schoolLocation: '',
-        titleOfStudy: '',
-        gpa: '',
-        fromDate: '',
-        toDate: '',
-    },
-    experience: [
-        {   
-            jobId: 1,
-            companyName: '',
-            location: '',
-            positionTitle: '',
-            responsibilities: [''],
-            dateFrom: '',
-            dateUntil: '',
-        }
-    ],
+  ],
 };
 
 const exampleFormData = {
-    profile: {
-        name: 'Vincent Tanuwidjaja',
-        email: 'vincentanu04@gmail.com',
-        phoneNumber: '(60) 11 2832 3824',
-        location: 'Subang Jaya, Malaysia',
+  profile: {
+    name: 'Vincent Tanuwidjaja',
+    email: 'vincentanu04@gmail.com',
+    phoneNumber: '(60) 11 2832 3824',
+    location: 'Subang Jaya, Malaysia',
+  },
+  education: {
+    schoolName: 'Monash University Malaysia',
+    schoolLocation: 'Subang Jaya, Malaysia',
+    titleOfStudy: "Bachelor's in Computer Science",
+    gpa: '3.8/4',
+    fromDate: '23/02/2022',
+    toDate: '23/11/2024',
+  },
+  experience: [
+    {
+      jobId: 1,
+      companyName: 'MHub Malaysia',
+      location: 'Puchong, Malaysia',
+      positionTitle: 'Web Developer Intern',
+      responsibilities: ['Writing new React components'],
+      dateFrom: '15/11/2023',
+      dateUntil: '23/02/2024',
     },
-    education: {
-        schoolName: 'Monash University Malaysia',
-        schoolLocation: 'Subang Jaya, Malaysia',
-        titleOfStudy: "Bachelor's in Computer Science",
-        gpa: '3.8/4',
-        fromDate: '23/02/2022',
-        toDate: '23/11/2024',
+    {
+      jobId: 2,
+      companyName: 'Tiger Sugar Malaysia',
+      location: 'Bandar Sunway, Malaysia',
+      positionTitle: 'Barista',
+      responsibilities: ['Training new employees'],
+      dateFrom: '15/11/2022',
+      dateUntil: '23/02/2023',
     },
-    experience: [
-        {   
-            jobId: 1,
-            companyName: 'MHub Malaysia',
-            location: 'Puchong, Malaysia',
-            positionTitle: 'Web Developer Intern',
-            responsibilities: ['Writing new React components'],
-            dateFrom: '15/11/2023',
-            dateUntil: '23/02/2024',
-        },
-        {
-            jobId: 2,
-            companyName: 'Tiger Sugar Malaysia',
-            location: 'Bandar Sunway, Malaysia',
-            positionTitle: 'Barista',
-            responsibilities: ['Training new employees'],
-            dateFrom: '15/11/2022',
-            dateUntil: '23/02/2023',
-        },
-        {
-            jobId: 3,
-            companyName: 'Robogals Monash Malaysia',
-            location: 'Bandar Sunway, Malaysia',
-            positionTitle: 'Event Organizer',
-            responsibilities: ['Organizing events'],
-            dateFrom: '15/11/2022',
-            dateUntil: 'Present',
-        }
-    ],
-}
+    {
+      jobId: 3,
+      companyName: 'Robogals Monash Malaysia',
+      location: 'Bandar Sunway, Malaysia',
+      positionTitle: 'Event Organizer',
+      responsibilities: ['Organizing events'],
+      dateFrom: '15/11/2022',
+      dateUntil: 'Present',
+    },
+  ],
+};
 
 function App() {
-    const [selectedButtonId, setSelectedButtonId] = useState(0);
-    const [formData, setFormData] = useState(initialFormData);
-    const [displayedData, setDisplayedData] = useState(initialFormData)
+  const [selectedButtonId, setSelectedButtonId] = useState(0);
+  const [formData, setFormData] = useState(initialFormData);
+  const [displayedData, setDisplayedData] = useState(initialFormData);
 
-    function handleNavButtonClick(id) {
-        setSelectedButtonId(id);
-    }
+  function handleNavButtonClick(id) {
+    setSelectedButtonId(id);
+  }
 
-    const formsData = [
-        { id: 0, text: "Profile", formComponent: ProfileForm},
-        { id: 1, text: "Education" , formComponent: EducationForm},
-        { id: 2, text: "Experience", formComponent: ExperienceForm}
-    ];
+  const formsData = [
+    { id: 0, text: 'Profile', formComponent: ProfileForm },
+    { id: 1, text: 'Education', formComponent: EducationForm },
+    { id: 2, text: 'Experience', formComponent: ExperienceForm },
+  ];
 
-    const SelectedFormComponent = formsData[selectedButtonId].formComponent;
-    const formType = formsData[selectedButtonId].text.toLowerCase();
+  const SelectedFormComponent = formsData[selectedButtonId].formComponent;
+  const formType = formsData[selectedButtonId].text.toLowerCase();
 
-    return (
-        <main>
-            <div className='buttons-bar'>
-                <Buttons>
-                    {formsData.map(button => (
-                        <NavButton 
-                        key={button.id} 
-                        text={button.text} 
-                        isSelected={selectedButtonId === button.id} 
-                        onClick={() => handleNavButtonClick(button.id)}>
-                        </NavButton>
-                    ))}
-                </Buttons>
-                <Button text="Create" onClick={() => setDisplayedData(formData)}/>
-                <Buttons>
-                    <Button 
-                    text="Example" 
-                    onClick={() => {
-                        setFormData((prevData) => {
-                            const newData = exampleFormData;
-                            setDisplayedData(newData);
-                            return newData;
-                        });
-                    }}/>
-                    <Button 
-                    text="Reset" 
-                    onClick={() => {
-                        setFormData((prevData) => {
-                          const newData = initialFormData;
-                          setDisplayedData(newData);
-                          return newData;
-                        });
-                    }}/>
-                </Buttons>
-            </div>
-            <div className="form-container">
-                <SelectedFormComponent
-                formValue={formData[formType]}
-                onChange={newFormValue => setFormData(prevFormValue => (
-                    {
-                        ...prevFormValue,
-                        [formType]: newFormValue
-                    }
-                ))}/>
-            </div>
-            <CV profile={displayedData.profile} education={displayedData.education} experience={displayedData.experience}/>
-        </main>
-    )
+  const styles = StyleSheet.create({
+    viewer: {
+      flex: 'auto',
+      border: 0,
+      padding: 0,
+      background:
+        'linear-gradient(90deg, var(--background-clr), var(--secondary-clr) 75%)',
+    },
+  });
+
+  return (
+    <main>
+      <div className='buttons-bar'>
+        <Buttons>
+          {formsData.map((button) => (
+            <NavButton
+              key={button.id}
+              text={button.text}
+              isSelected={selectedButtonId === button.id}
+              onClick={() => handleNavButtonClick(button.id)}
+            ></NavButton>
+          ))}
+        </Buttons>
+        <Button text='Create' onClick={() => setDisplayedData(formData)} />
+        <Buttons>
+          <Button
+            text='Example'
+            onClick={() => {
+              setFormData((prevData) => {
+                const newData = exampleFormData;
+                setDisplayedData(newData);
+                return newData;
+              });
+            }}
+          />
+          <Button
+            text='Reset'
+            onClick={() => {
+              setFormData((prevData) => {
+                const newData = initialFormData;
+                setDisplayedData(newData);
+                return newData;
+              });
+            }}
+          />
+        </Buttons>
+      </div>
+      <div className='form-container'>
+        <SelectedFormComponent
+          formValue={formData[formType]}
+          onChange={(newFormValue) =>
+            setFormData((prevFormValue) => ({
+              ...prevFormValue,
+              [formType]: newFormValue,
+            }))
+          }
+        />
+      </div>
+      <PDFViewer style={styles.viewer}>
+        <CV
+          profile={displayedData.profile}
+          education={displayedData.education}
+          experience={displayedData.experience}
+        />
+      </PDFViewer>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/components/CV/CV.jsx
+++ src/components/CV/CV.jsx
@@ -1,77 +1,104 @@
-import './CV.css'
+import { Document, Page, StyleSheet, Text } from '@react-pdf/renderer';
+import './CV.css';
 
 export default function CV({ profile, education, experience }) {
-    const { name, email, phoneNumber, location } = profile;
-    const { schoolName, schoolLocation, titleOfStudy, gpa, fromDate, toDate} = education;
+  const { name, email, phoneNumber, location } = profile;
+  const { schoolName, schoolLocation, titleOfStudy, gpa, fromDate, toDate } =
+    education;
 
-    return (
-        <div className="cv">
-            <div className="paper">
-                <div className="profile">
-                    <h2>{name}</h2>
-                    <div className="profile-subtitle">
-                        <p>{location}</p>
-                        <p>{email}</p>
-                        <p>{phoneNumber}</p>
-                    </div>
-                </div>
-                {Object.values(education).some(value => value !== '') && (
-                    <div className="education">
-                        <h3>Education</h3>
-                        <hr />
-                        <div className="subtitle">
-                            <h3 className="subtitle-left">{schoolLocation}</h3>
-                            <h3 className='subtitle-center'>{schoolName}</h3>
-                            <h3 className='subtitle-right'>{fromDate} - {toDate}</h3>
-                        </div>
-                        <ul className="content">
-                            {titleOfStudy !== '' && (
-                                <li>
-                                <p>&bull; {titleOfStudy}</p>
-                                </li>
-                            )}
-                            {gpa !== '' && (
-                                <li>
-                                    <p>&bull; <b>GPA: </b>{gpa}</p>
-                                </li>
-                            )}
-                        </ul>
-                    </div>
-                )}
-                {Object.entries(experience[0]).some(([key, value]) => {
-                    if (key === "jobId") return false
-                    else if (key === "responsibilities") {
-                        return value[0] !== '';
-                    }
-                    else return value !== '';
-                }) && (
-                    <div className="experience">
-                        <h3>Work Experience</h3>
-                        <hr />
-                        {experience.map(job => {
-                            return (
-                                <div key={job} className='job-cv'>
-                                    <div className="subtitle">
-                                        <h3 className='subtitle-left'>{job.positionTitle}</h3>
-                                        <h3 className='subtitle-center'>{job.companyName}</h3>
-                                        <h3 className='subtitle-right'>{job.dateFrom} {(job.dateFrom || job.dateFrom) && "-"} {job.dateUntil}</h3>
-                                    </div>
-                                    <p>{job.location}</p>
-                                    <ul className="content">
-                                        {job.responsibilities.map(responsibility => responsibility !== '' && (
-                                            <li key={responsibility}>
-                                                <p>
-                                                    &bull; {responsibility}
-                                                </p>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            );
-                        })}
-                    </div>
-                )}
-            </div>
-        </div>
-    )
+  const styles = StyleSheet.create({
+    page: {
+      padding: '3rem',
+      display: 'grid',
+      placeItems: 'center',
+    },
+  });
+
+  return (
+    <Document
+      title={profile.name ? `${profile.name}_resume` : 'resume'}
+      pageLayout='singlePage'
+      pageMode='fullScreen'
+    >
+      <Page size='A4' orientation='portrait' style={styles.page}>
+        <Text>HELLOOO</Text>
+      </Page>
+    </Document>
+    // <div className='cv'>
+    //   <div className='paper'>
+    //     <div className='profile'>
+    //       <h2>{name}</h2>
+    //       <div className='profile-subtitle'>
+    //         <p>{location}</p>
+    //         <p>{email}</p>
+    //         <p>{phoneNumber}</p>
+    //       </div>
+    //     </div>
+    //     {Object.values(education).some((value) => value !== '') && (
+    //       <div className='education'>
+    //         <h3>Education</h3>
+    //         <hr />
+    //         <div className='subtitle'>
+    //           <h3 className='subtitle-left'>{schoolLocation}</h3>
+    //           <h3 className='subtitle-center'>{schoolName}</h3>
+    //           <h3 className='subtitle-right'>
+    //             {fromDate} - {toDate}
+    //           </h3>
+    //         </div>
+    //         <ul className='content'>
+    //           {titleOfStudy !== '' && (
+    //             <li>
+    //               <p>&bull; {titleOfStudy}</p>
+    //             </li>
+    //           )}
+    //           {gpa !== '' && (
+    //             <li>
+    //               <p>
+    //                 &bull; <b>GPA: </b>
+    //                 {gpa}
+    //               </p>
+    //             </li>
+    //           )}
+    //         </ul>
+    //       </div>
+    //     )}
+    //     {Object.entries(experience[0]).some(([key, value]) => {
+    //       if (key === 'jobId') return false;
+    //       else if (key === 'responsibilities') {
+    //         return value[0] !== '';
+    //       } else return value !== '';
+    //     }) && (
+    //       <div className='experience'>
+    //         <h3>Work Experience</h3>
+    //         <hr />
+    //         {experience.map((job) => {
+    //           return (
+    //             <div key={job} className='job-cv'>
+    //               <div className='subtitle'>
+    //                 <h3 className='subtitle-left'>{job.positionTitle}</h3>
+    //                 <h3 className='subtitle-center'>{job.companyName}</h3>
+    //                 <h3 className='subtitle-right'>
+    //                   {job.dateFrom} {(job.dateFrom || job.dateFrom) && '-'}{' '}
+    //                   {job.dateUntil}
+    //                 </h3>
+    //               </div>
+    //               <p>{job.location}</p>
+    //               <ul className='content'>
+    //                 {job.responsibilities.map(
+    //                   (responsibility) =>
+    //                     responsibility !== '' && (
+    //                       <li key={responsibility}>
+    //                         <p>&bull; {responsibility}</p>
+    //                       </li>
+    //                     )
+    //                 )}
+    //               </ul>
+    //             </div>
+    //           );
+    //         })}
+    //       </div>
+    //     )}
+    //   </div>
+    // </div>
+  );
 }
