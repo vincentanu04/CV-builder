725e7e60d14b903c29afdead73a045cba5e2a3ba
vincentanu04 <vincentanu04@gmail.com>
2024-05-13 16:44:12 +0800

    add add and remove button in multipleinput

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,19 +1,36 @@
-import './Buttons.css'
+import './Buttons.css';
 
 export default function Buttons({ children }) {
-    return (
-        <div className='buttons'>{children}</div>
-    )
+  return <div className='buttons'>{children}</div>;
 }
 
 export function Button({ text, onClick, disabled }) {
-    return (
-        <button className='button' onClick={onClick} disabled={disabled}>{text}</button>
-    )
+  return (
+    <button className='button' onClick={onClick} disabled={disabled}>
+      {text}
+    </button>
+  );
+}
+
+export function MultipleInputButton({ text, onClick, disabled }) {
+  return (
+    <button
+      className='multiple-input-button'
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
 }
 
 export function NavButton({ text, isSelected, onClick }) {
-    return (
-        <button className={"nav-button " + (isSelected ? "selected" : null)} onClick={onClick}>{text}</button>
-    )
+  return (
+    <button
+      className={'nav-button ' + (isSelected ? 'selected' : null)}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 }
--- src/components/Form/components/MultipleInput.jsx
+++ src/components/Form/components/MultipleInput.jsx
@@ -1,3 +1,5 @@
+import Buttons, { MultipleInputButton } from '../../Buttons/Buttons';
+
 const MultipleInput = ({
   array,
   onChange,
@@ -6,20 +8,44 @@ const MultipleInput = ({
   formValue,
 }) => {
   return array.map((value, index) => (
-    <input
-      key={index}
-      type='text'
-      name={formValueKey}
-      value={value}
-      placeholder={`${placeholder} ${index + 1}`}
-      onChange={(e) => {
-        const updatedFormValue = { ...formValue };
-        const updatedArray = [...updatedFormValue[formValueKey]];
-        updatedArray[index] = e.target.value;
-        updatedFormValue[formValueKey] = updatedArray;
-        onChange(updatedFormValue);
-      }}
-    />
+    <div key={index} className='multiple-input'>
+      <input
+        type='text'
+        name={formValueKey}
+        value={value}
+        placeholder={placeholder}
+        onChange={(e) => {
+          const updatedFormValue = { ...formValue };
+          const updatedArray = [...updatedFormValue[formValueKey]];
+          updatedArray[index] = e.target.value;
+          updatedFormValue[formValueKey] = updatedArray;
+          onChange(updatedFormValue);
+        }}
+      />
+      <Buttons>
+        <MultipleInputButton
+          text='+'
+          onClick={() => {
+            const updatedFormValue = { ...formValue };
+            const updatedArray = [...updatedFormValue[formValueKey]];
+            updatedArray.splice(index + 1, 0, '');
+            updatedFormValue[formValueKey] = updatedArray;
+            onChange(updatedFormValue);
+          }}
+        />
+        <MultipleInputButton
+          text='-'
+          disabled={array.length > 1 ? false : true}
+          onClick={() => {
+            const updatedFormValue = { ...formValue };
+            const updatedArray = [...updatedFormValue[formValueKey]];
+            updatedArray.splice(index, 1);
+            updatedFormValue[formValueKey] = updatedArray;
+            onChange(updatedFormValue);
+          }}
+        />
+      </Buttons>
+    </div>
   ));
 };
 
