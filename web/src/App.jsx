7c442cbf3ff4ca5812d2d3e73717a0720a26682c
vincentanu04 <vincentanu04@gmail.com>
2025-01-10 19:39:39 +0700

    add cd server to backend ci

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -6,9 +6,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<ResumeForm />} />
-        {/* <Route path='/' element={<LandingPage />} /> */}
-        {/* <Route path='/resume' element={<ResumeForm />} /> */}
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/resume' element={<ResumeForm />} />
       </Routes>
     </Router>
   );
