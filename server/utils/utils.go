d94c5ac679ff312f691387c5c150797699508c7f
vincentanu04 <vincentanu04@gmail.com>
2025-01-11 03:47:40 +0700

    fix unchecked error on writejson

--- server/utils/utils.go
+++ server/utils/utils.go
@@ -13,7 +13,10 @@ var Validate = validator.New()
 func WriteJSON(w http.ResponseWriter, status int, v any) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(v)
+
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		http.Error(w, "Failed to encode JSON response", http.StatusInternalServerError)
+	}
 }
 
 func ParseJSON(r *http.Request, v any) error {
