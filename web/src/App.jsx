377307ee06f8fec2f6856e8485de04ec705c5bf8
vincentanu04 <vincentanu04@gmail.com>
2025-01-13 16:09:56 +0700

    git ignore env

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<ResumeForm />} />
-        {/* <Route path='/resume' element={<ResumeForm />} /> */}
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/resume' element={<ResumeForm />} />
       </Routes>
     </Router>
   );
