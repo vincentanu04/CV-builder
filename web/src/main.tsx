6b8744e61fa19d667597630df0cfeafa4ae2897f
vincentanu04 <vincentanu04@gmail.com>
2025-02-06 18:02:55 +0700

    build: migrate from js to ts for web

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,12 @@
+/// <reference types="vite/client" />
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App';
 import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId=''>
       <App />
