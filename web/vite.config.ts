6cc39d29c386aab018a1d949bb3f3d7c70eddee4
vincentanu04 <vincentanu04@gmail.com>
2025-02-08 01:51:29 +0700

    feat: add tailwindcss

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'url';
 import path from 'path';
-
-// Define __dirname using import.meta.url
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Alias '@' to the 'src' folder
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
